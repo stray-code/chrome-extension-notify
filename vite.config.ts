87d3a3bceb28e7617ce3f0ba219699735231c7e1
stray-code <abc1007020@gmail.com>
2024-04-20 14:36:16 +0900

    crxjs追加と設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "指定時間に通知",
+  description: "指定時間に通知を表示します。",
+  version: "1.0.0",
+  icons: {
+    16: "img/icon16.png",
+    48: "img/icon48.png",
+    128: "img/icon128.png",
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
+});
