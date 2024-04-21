6b6e41e591213b8aeecaac15d06c5743a2d45f36
stray-code <abc1007020@gmail.com>
2024-04-21 12:07:46 +0900

    description修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { crx, defineManifest } from "@crxjs/vite-plugin";
 const manifest = defineManifest({
   manifest_version: 3,
   name: "指定時間に通知",
-  description: "指定時間に通知を表示します。",
+  description: "指定時間に通知します。",
   version: "1.0.0",
   icons: {
     16: "img/icon16.png",
