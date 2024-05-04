7c2fdc358333b9665448d87e293850525711e585
stray-code <abc1007020@gmail.com>
2024-05-04 20:47:47 +0900

    バージョン更新

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const manifest = defineManifest({
   manifest_version: 3,
   name: "指定時間に通知",
   description: "指定時間に通知します。",
-  version: "1.1.0",
+  version: "1.1.1",
   icons: {
     16: "img/icon16.png",
     48: "img/icon48.png",
